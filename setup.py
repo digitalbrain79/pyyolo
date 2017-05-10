eaccd43d16d1c2fc9d6dbf8b9e168164bd36042f
Oytun Ulutan <ulutan@ece.ucsb.edu>
2017-05-10 14:24:28 -0700

    Fixes numpy include and changes makefile to not use cudnn

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup, Extension
+import numpy
 
 module = Extension('pyyolo',
 	library_dirs=['.', '/usr/local/cuda/lib64', '/usr/local/'],
 	libraries=['yolo', 'cuda', 'cudart', 'cublas', 'curand', 'cudnn'],
+	include_dirs=[numpy.get_include()],
 	sources = ['module.c'])
 
 setup (name = 'pyyolo',
