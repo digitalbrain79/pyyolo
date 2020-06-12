dc8533516289e28170bd861a7d830e4d9fa72512
Deyong Zheng <zhengdy@me.com>
2020-06-12 22:26:10 +0900

    fix import pyyolo error. undefined symbol: _ZGVbN2vv___pow_finite

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import numpy
 module = Extension('pyyolo',
 	library_dirs=['.'],
 	libraries=['yolo'],
+    extra_compile_args=['-fopenmp'],
+    extra_link_args=['-lgomp', '-lm', '-lmvec'],
 	include_dirs=[numpy.get_include(), './darknet/include'],
 	sources = ['module.c'])
 
